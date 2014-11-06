024fe04cf33cc31547f0f8663464bdf8d80f85ae
shrkw <shrkwh@gmail.com>
2014-11-06 15:34:48 -0500

    Modify Brewfile bash-runnable

---
+++ Brewfile.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+brew update
+brew upgrade
+
+brew install coreutils
+brew install readline
+brew install zsh
+brew install vim --with-lua
+brew install tmux
+brew install tree
+brew install colordiff
+brew install git
+brew install tig
+#brew install scala
+brew install sbt
+brew install unrar
+brew install p7zip
+
+brew install peco/peco/peco
+brew install motemen/ghq/ghq
+
+# Cask
+brew install caskroom/cask/brew-cask
+
+brew cask install clipmenu
+brew cask install virtualbox
+brew cask install vagrant
+#cask install firefox
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install skype
+brew cask install brackets
+#cask install sublime-text3
+#cask install xquartz
+#install sanemat/font/ricty
+
+brew cleanup
