0f6084f84bf8b18be303d7a9abeab6668bbae5f9
shrkw <shrkwh@gmail.com>
2016-10-16 21:25:48 +0900

    Leave cask, update formulas

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,42 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 brew update
 brew upgrade
 
-brew install coreutils
-brew install readline
-brew install zsh
+FORMULAS=( \
+coreutils readline colordiff tree \
+zsh tmux reattach-to-user-namespace \
+git tig peco ghq jq direnv \
+pyenv rbenv ruby-build \
+unrar p7zip xz \
+)
+
+for f in ${FORMULAS[@]}; do
+  brew install $f
+done
+
 brew install vim --with-lua
-brew install tmux
-brew install reattach-to-user-namespace
-brew install jq
-brew install tree
-brew install colordiff
-brew install git
-brew install tig
-#brew install scala
-brew install sbt
-brew install unrar
-brew install p7zip
-brew install peco
-brew install ghq
 
-# Cask
-brew install caskroom/cask/brew-cask
-
-brew cask install clipmenu
-brew cask install virtualbox
-brew cask install vagrant
-#cask install firefox
-#brew cask install google-chrome
-#brew cask install dropbox
-#brew cask install skype
-#brew cask install brackets
-#cask install sublime-text3
-#cask install xquartz
-#install sanemat/font/ricty
-
-brew cleanup
-brew cask cleanup
