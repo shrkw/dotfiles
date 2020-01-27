c315262eac0a108360840b3400aef68bdf578327
shrkw <shrkwh@gmail.com>
2020-01-27 14:15:58 +0900

    add cask formulas

--- Brewfile.sh
+++ Brewfile.sh
@@ -22,3 +22,17 @@ tmux \
 for f in ${FORMULAS[@]}; do
   brew install $f
 done
+
+CASK_FORMULAS=( \
+google-chrome \
+firefox \
+clipy \
+iterm2 \
+docker \
+visual-studio-code \
+the-unarchiver \
+)
+
+for f in ${CASK_FORMULAS[@]}; do
+  brew cask install $f
+done
