cf20cb73613bd735d0eeb22bc43661635e2c1470
shrkw <shrkwh@gmail.com>
2022-01-21 21:52:06 +0900

    remove about homebrew

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 set -ue
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install ghq
 ghq get https://github.com/shrkw/dotfiles.git
 cd ~/ghq/github.com/shrkw/dotfiles
