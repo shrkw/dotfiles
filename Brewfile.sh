0f1b1078fd3623b1d282a5d1ccd69915aff03bd5
shrkw <shrkwh@gmail.com>
2022-01-21 21:51:09 +0900

    remove casks

--- Brewfile.sh
+++ Brewfile.sh
@@ -23,17 +23,3 @@ zsh-completions \
 for f in ${FORMULAE[@]}; do
   brew install $f
 done
-
-CASK_FORMULAE=( \
-google-chrome \
-firefox \
-clipy \
-iterm2 \
-docker \
-visual-studio-code \
-the-unarchiver \
-)
-
-for f in ${CASK_FORMULAE[@]}; do
-  brew cask install $f
-done
